92c3d2f31b8444b556ac8da1fc4c796932f9d498
andreevnikola <andr.nikola.08@gmail.com>
2025-03-29 20:31:40 +0200

    a little progress

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,11 +4,15 @@ import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { AppService } from './app.service';
 import { HeatmapDto } from './dto/heatmap.dto';
 import { ScheduleRequirementsDTO } from './dto/ScheduleRequirements.dto';
+import { ScheduleService } from './schedule.service';
 
 @ApiTags('satellites')
-@Controller('satellites')
+@Controller('v01')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly scheduleService: ScheduleService,
+  ) {}
 
   @Post('heatmap')
   @ApiOperation({
@@ -25,6 +29,6 @@ export class AppController {
       'Returns commercial launches of rockets to which you can attach your payload in ordre for it to be sent to space',
   })
   async schedule(@Body() requirements: ScheduleRequirementsDTO) {
-    return this.appService.schedule({ ...requirements });
+    return this.scheduleService.schedule({ ...requirements });
   }
 }
--- src/dto/ScheduleRequirements.dto.ts
+++ src/dto/ScheduleRequirements.dto.ts
@@ -1,11 +1,52 @@
-export interface ScheduleRequirementsDTO {
-  time_frame: {
-    start: string;
-    end: string;
-  };
-  orbit: 'LEO' | 'MEO' | 'GEO';
-  point_of_interest?: {
-    latitude: number;
-    longitude: number;
-  };
+import { Type } from 'class-transformer';
+import {
+  IsNotEmpty,
+  IsNotEmptyObject,
+  IsNumberString,
+  IsObject,
+  IsOptional,
+  IsString,
+  ValidateNested,
+} from 'class-validator';
+
+export class PointOfInterest {
+  @IsNotEmpty()
+  @IsNumberString()
+  latitude: string;
+
+  @IsNotEmpty()
+  @IsNumberString()
+  longitude: string;
+}
+
+export class TImeFrame {
+  @IsNotEmpty()
+  @IsString()
+  start: string;
+
+  @IsNotEmpty()
+  @IsString()
+  end: string;
+}
+
+export class ScheduleRequirementsDTO {
+  @IsNotEmptyObject()
+  @ValidateNested()
+  @Type(() => TImeFrame)
+  time_frame: TImeFrame;
+
+  @IsNotEmpty()
+  @IsString()
+  orbit: string;
+
+  @IsOptional()
+  @ValidateNested()
+  @Type(() => PointOfInterest)
+  point_of_interest?: PointOfInterest;
+}
+
+export class PointOfOrbiting {
+  latitude: number;
+  longitude: number;
+  altitude?: number;
 }
